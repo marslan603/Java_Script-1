c69f6ed9aceb605b8fefb5bf5e52abca15bea206
Mücahit Arslan <marslan603@gmail.com>
2022-08-06 14:07:56 +0300

    Done

--- app.js
+++ app.js
@@ -3,5 +3,29 @@ console.log('app.js is loaded');
 // let firstName = prompt('Enter your name:');
 // alert('firstName :' + firstName);
 
-let sonuc = confirm('are you legal?');
-console.log(sonuc);
+// let sonuc = confirm('are you legal?');
+// console.log(sonuc);
+
+// var myNumber1 = 11;
+// console.log(myNumber1);
+
+// var myNumber1 = 22;
+// console.log(myNumber1);
+
+// let.v1=88;
+// console.log(v1);
+
+// let.v2 = 77
+// console.log(v2);
+
+const student = {
+    name: 'Nedim',
+    age: 34,
+};
+
+console.log(student);
+
+const otherStudent = student;
+    otherStudent.name= 'Mucahit';
+    otherStudent.age= 29;
+    console.log(otherStudent);
